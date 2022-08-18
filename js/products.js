7a7081185908dac964bd9f245aac4b6bb025d8cf
iBACH48 <machadito48@gmail.com>
2022-08-18 06:36:11 -0300

    parte 2

---
+++ js/products.js
@@ -0,0 +1,40 @@
+let productsArray = [];
+
+function showProductsList(array){
+    let htmlContentToAppend = "";
+/* precio, nombre, descripción, cantidad vendidos e imagen */
+    for(let i = 0; i < array.products.length; i++){ 
+        let products = array.products[i];
+        htmlContentToAppend += `
+        
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ products.name + " - " + products.currency + products.cost +`</h4> 
+                        <p> `+ products.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + products.soldCount + ` Artículos vendidos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            showProductsList(productsArray);
+        }
+    });
+});
